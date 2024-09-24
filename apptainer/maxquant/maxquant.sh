6309bdeb2bcfd282134c0efd3a67687bd1de0e18
aharral <aharral@wistar.org>
2024-09-24 12:47:55 -0400

    added comments for what to change

--- apptainer/maxquant/maxquant.sh
+++ apptainer/maxquant/maxquant.sh
@@ -2,7 +2,10 @@
 #SBATCH --job-name=maxquant
 #SBATCH --mem=100G
 #SBATCH --ntasks=1
+
+### UPDATE THIS NUMBER TO MATCH THE <numThreads> IN mqpar.xml
 #SBATCH --cpus-per-task=4
+
 #SBATCH --time=01:00:00
 #SBATCH --mail-user=aharral@wistar.org
 #SBATCH --mail-type=ALL
@@ -11,12 +14,12 @@
 
 module load apptainer
 
-export OUT_PATH="it/aharral/maxquant_out_data"
 
 # Use this to ensure you do not pull an already existing container
+# UPDATE THIS .sif FILE NAME IF YOU WANT TO CHANGE IT (optional)
 if ! test -f maxquant.sif; then
     echo "Container does not exist...building"
-    apptainer build --bind=/wistar/:/mnt/ maxquant.sif maxquant.def
+    apptainer build --bind=/applications/maxquant/:/mnt/ maxquant.sif maxquant.def
     echo "Container maxquant.sif was build successfully"
 else
     echo "Container already exists, please delete it and run again."
@@ -27,7 +30,9 @@ fi
 apptainer run --writable-tmpfs maxquant.sif
 
 # Copy out data
-apptainer --bind=/wistar/:/mnt/ exec cp -r /opt/data /mnt/$OUT_PATH
+# UPDATE THIS PATH TO AN OUTPUT DIRECTORY
+export OUT_PATH="/path/to/output"
+apptainer exec cp -r /opt/data $OUT_PATH
 
-# Clean up
+# Clean up apptainer
 apptainer cache clean --force
