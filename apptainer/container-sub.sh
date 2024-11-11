6e391faa67c36d1b967ddc64f5f753bd9725233e
aharral <aharral@wistar.org>
2024-11-11 14:12:03 -0500

    moved to apptainer folder

---
+++ apptainer/container-sub.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=sample               # create a short name for your job
+#SBATCH --partition=defq                # shared partition (queue)
+#SBATCH --nodes=1                       # node count
+#SBATCH --ntasks=2                      # total number of tasks across all nodes
+#SBATCH --cpus-per-task=1               # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem=3GB                       # total memory allocated for all tasks
+#SBATCH --time=00:01:00                 # total run time limit (HH:MM:SS)
+#SBATCH --mail-type=begin,end,fail      # send email when job begins,ends,fails
+#SBATCH --mail-user=username@wistar.org # email to send alerts to
+#SBATCH --output slurm.%N.%j.out        # Output file name and Location
+#SBATCH --error slurm.%N.%j.err         # Error file name and Location
+
+# Load the apptainer module
+module load apptainer
+
+# if the .sif file does not exist, build/pull it
+if ! test -f container.sif; then
+    echo "Container does not exist...building"
+    apptainer pull docker://image
+    apptainer build container.sif recipe.def
+fi
+
+# execute command within image
+apptainer exec container.sif <command>
