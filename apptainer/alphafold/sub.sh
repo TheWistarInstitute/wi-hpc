57833fa8b64b2d40da41b274d8d0c868d8d56647
aharral <aharral@wistar.org>
2024-11-21 10:40:52 -0500

    updated sub file

--- apptainer/alphafold/sub.sh
+++ apptainer/alphafold/sub.sh
@@ -6,16 +6,13 @@
 #SBATCH --partition=gpu
 
 # Rest of standard config
-#SBATCH --job-name=sample               # job name
-#SBATCH --nodes=1                       # node count
-#SBATCH --ntasks=2                      # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1               # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --job-name=alphafold3_test               # job name
 #SBATCH --mem=1GB                       # total memory allocated for all tasks
 #SBATCH --time=00:01:00                 # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin,end,fail      # send email when job begins,ends,fails
 #SBATCH --mail-user=username@wistar.org # email to send alerts to
-#SBATCH --output slurm.%N.%j.out        # Output file name and Location
-#SBATCH --error slurm.%N.%j.err         # Error file name and Location
+#SBATCH --output=slurm.%N.%j.out        # Output file name and Location
+#SBATCH --error=slurm.%N.%j.err         # Error file name and Location
 
 # Load Required Modules
 module load apptainer
