dcebd87e2e9bea5125fc1bb38dd02adfd58cfeba
aharral <aharral@wistar.org>
2024-11-26 11:52:55 -0500

    updated container sub fle

--- apptainer/sub.sh
+++ apptainer/sub.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
+#SBATCH --job-name=sample
+#SBATCH --mem=3GB
 
-# Template Submission script for use with apptainer containers and defintions files
-
-#SBATCH --job-name=sample               # create a short name for your job
-#SBATCH --partition=defq                # shared partition (queue)
-#SBATCH --nodes=1                       # node count
-#SBATCH --ntasks=2                      # total number of tasks across all nodes
-#SBATCH --cpus-per-task=1               # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem=3GB                       # total memory allocated for all tasks
-#SBATCH --time=00:01:00                 # total run time limit (HH:MM:SS)
-#SBATCH --mail-type=begin,end,fail      # send email when job begins,ends,fails
-#SBATCH --mail-user=username@wistar.org # email to send alerts to
-#SBATCH --output slurm.%N.%j.out        # Output file name and Location
-#SBATCH --error slurm.%N.%j.err         # Error file name and Location
-
-# Load the apptainer module
 module load apptainer
 
 # if the .sif file does not exist, build/pull it
@@ -24,4 +11,7 @@ if ! test -f container.sif; then
 fi
 
 # execute command within image
-apptainer exec container.sif <command>
+apptainer exec container.sif commmand --args
+
+# or run the container with default run command (see %runscript section of definition file)
+apptainer run container.sif --args
