51e801ed811610404d88a11bf6063449219dbc8f
aharral <aharral@wistar.org>
2025-05-16 11:14:37 -0400

    updates

--- apptainer/jupyter/sub.sh
+++ apptainer/jupyter/sub.sh
@@ -12,26 +12,27 @@
 #SBATCH --error slurm-%j.err         # Error file name and Location
 
 # generate a random port
-port=$(shuf -i 6000-9999 -n 1)
+PORT=$(shuf -i 6000-9999 -n 1)
 
 # define container name
-container=base-notebook:latest
+IMAGE=scipy-notebook:latest
+CONTAINER=scipy-notebook_latest.sif
 
 # load apptainer module
 module load apptainer
 
 # if the .sif file does not exist, pull it
-if ! test -f $container; then
-  echo "Container does not exist...pulling"
-  apptainer pull docker://jupyter/$container
+if ! test -f $IMAGE; then
+  echo "Image does not exist...pulling"
+  apptainer pull docker://jupyter/$IMAGE
 fi
 
 # forward port to head node
-apptainer exec $container /usr/bin/ssh -N -f -R $port:localhost:$port wi-hpc
+apptainer exec $CONTAINER /usr/bin/ssh -N -f -R $PORT:localhost:$PORT wi-hpc
 
 # start the notebook
-apptainer exec $container jupyter-notebook --no-browser --port=$port
+apptainer exec $CONTAINER jupyter-notebook --no-browser --port=$PORT
 
 echo "run the following on your local machine:
-ssh -N -L 9999:localhost:9999 wi-hpc
+ssh -N -L $PORT:localhost:$PORT wi-hpc
 and then access the web ui through the link provided that starting with http://127.0.0.1"
