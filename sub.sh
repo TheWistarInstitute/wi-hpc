578b5585bd455241d4e3b4a00e510255ca292f53
aharral <aharral@wistar.org>
2025-05-19 11:01:24 -0400

    changed time

--- sub.sh
+++ sub.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1               # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=1G                        # total memory allocated for all tasks
 #SBATCH --mem-per-cpu=1G                # allocate memory based on # of cpus
-#SBATCH --time=00:01:00                 # total run time limit (HH:MM:SS)
+#SBATCH --time=1:00:00                 # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=begin,end,fail      # send email when job begins,ends,fails
 #SBATCH --mail-user=username@wistar.org # email to send alerts to
 #SBATCH --output slurm.%N.%j.out        # Output file name and Location
